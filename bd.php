948a9bf54dc67b5bcb9e87e04c0efb324bb1b1f2
Alexis Lécuyer <alexis.lecuyer.01@edu.cegeptr.qc.ca>
2020-11-19 15:14:19 -0500

    avancement

---
+++ bd.php
@@ -0,0 +1,48 @@
+<?php
+try {
+    $connexion = new PDO("mysql:host=localhost;dbname=hello_garage;port=3308", "root", "");
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo json_encode("erreur" . $e->getMessage());
+}
+if ($_POST["requete"] == "insertUser") {
+
+    try {
+        $stmt = $connexion->prepare("INSERT INTO `users`( `Prenom`, `Nom`, `Courriel`, `Mdp`) VALUES (:prenom,:nom,:courriel,:mdp)");
+        $stmt->bindParam(':prenom', $_POST['prenom']);
+        $stmt->bindParam(':nom', $_POST['nom']);
+        $stmt->bindParam(':courriel', $_POST['courriel']);
+        $stmt->bindParam(':mdp', $_POST['mdp']);
+        $stmt->execute();
+
+    } catch (PDOException $e) {
+        echo json_encode($e);
+    }
+} elseif ($_POST["requete"] == "login") {
+    try {
+        $stmt = $connexion->prepare("SELECT * FROM USERS WHERE `Courriel` like :email");
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($results);
+    } catch (PDOException $e) {
+        echo json_encode($e);
+    }
+} elseif ($_POST["requete"] == "checkEmail") {
+    try {
+        $stmt = $connexion->prepare("SELECT * FROM USERS WHERE `Courriel` like :email");
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (sizeof($results) > 0) {
+            echo json_encode(false);
+        } else
+            echo json_encode(true);
+
+    } catch (PDOException $e) {
+        echo json_encode($e);
+    }
+}
+
+
+?>
